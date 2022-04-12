feb32161828db30f49f48bf73dfae14ad3d33785
AmirAnckonina <50033620+AmirAnckonina@users.noreply.github.com>
2022-04-12 12:15:31 +0300

    Update

--- DataStructures-Ex01/FindPathsProgram.h
+++ DataStructures-Ex01/FindPathsProgram.h
@@ -3,36 +3,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
-
 #include "Country.h"
 #include "AccessibleGroup.h"
 
-//#define NOT_VALID -1
+
+
+enum class eColors {BLACK, WHITE};
 
 class FindPathsProgram
 {
-
-private:
+public:
 	const int NOT_VALID = -1;
 
+
+private:
 	Country m_Country;
 	int m_CityCenter;
 
 public:
-
 	void run();
 	void InputProcedure();
+	//void BuildCitiesColorsArr(eColors& i_CitiesColors);
+	eColors* BuildCitiesColorsArr();
+	
 	AccessibleGroup GetToTownWrapper();
-	//void GetToTown();
+	void GetToTown(const Country& i_Country, const City& i_CurrCityCenter, eColors& io_CitiesColors, AccessibleGroup* io_AccessGrp);
 	void GetNumOfCitiesAndRoads();
 	void InitCityPairsRoadsFromUser(vector<pair<int, int>>& i_CityPairs, int i_NumOfRoads);
 	void GetCityCenterNumberFromUser();
 	int GetNumberFromIndexInString(string i_Str, int& index);
 	bool IsADigit(char ch);
 	bool IsRoadExist(vector<pair<int, int>> i_CityPairs, pair<int, int> i_RoadPair);
-
 	bool IsNumInRange(int num, int from, int to);
-
 	void InvalidExit();
 
 
